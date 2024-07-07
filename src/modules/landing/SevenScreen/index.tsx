e49d5f29a59cf3314ace630f06a3e046d41646b9
maxi-q <maximkabust@gmail.com>
2024-07-07 23:39:56 +0300

    docker

--- src/modules/landing/SevenScreen/index.tsx
+++ src/modules/landing/SevenScreen/index.tsx
@@ -7,30 +7,30 @@ import styles from './style.module.css'
 
 export const SevenScreen = () => {
   return (
-    <main className={'section p-5 lg:pl-[50px] lg:pt-[70px] flex gap-16 xl:gap-10 ' + styles.screen}>
-      <div className="lg:block flex flex-col" style={{ flex: 1 }}>
-        <h1 className="text-3xl lg:text-4xl xl:text-5xl uppercase w-max">
+    <main className={'section p-5 lg:pl-[50px] lg:pt-[70px] flex gap-16 xl:gap-10  ' + styles.screen}>
+      <div className="lg:block flex flex-col items-center" style={{ flex: 1 }}>
+        <h1 className="text:text-2xl md:text-3xl lg:text-4xl xl:text-5xl uppercase w-max">
           <span className={styles.titleSpan}>остались вопросы? </span> <br />
           Не знаете нужен ли вам <br />
           чат-бот/автоворонка?
         </h1>
-        <h3 className="font-bold text-base lg:text-lg uppercase pt-6 pb-10 tracking-wider w-max">
+        <h3 className="font-bold text-sm md:text-base lg:text-lg uppercase pt-6 pb-10 tracking-wider w-max">
           Оставьте заявку на бесплатную консультацию, <br />
           расскажите про свой проект и за 30 минут <br />
           определимся нужно ли вам это или нет
         </h3>
         <div className={'rounded-xl p-6 xl:p-14 w-max ' + styles.feedback}>
           <h4 className="uppercase text-lg mb-6 font-bold tracking-wider">Где с вами связаться?</h4>
-          <div className="flex gap-6">
-            <a className={'rounded-full px-8 py-6 xl:px-12 flex items-center text-base xl:text-lg uppercase font-medium  lg:gap-3 tracking-wide ' + styles.massagerLink}>
+          <div className="flex flex-col sm:flex-row gap-6">
+            <a className={'rounded-full py-2 px-4 sm:px-8 sm:py-6 xl:px-12 flex items-center text-base xl:text-lg uppercase font-medium  lg:gap-3 tracking-wide ' + styles.massagerLink}>
               <Telephone /> Позвонить
             </a>
-            <a className={'rounded-full px-8 py-6 xl:px-12 flex items-center text-base xl:text-lg uppercase font-medium gap-2 lg:gap-3 tracking-wide ' + styles.massagerLink}>
+            <a className={'rounded-full py-2 px-4 sm:px-8 sm:py-6 xl:px-12 flex items-center text-base xl:text-lg uppercase font-medium gap-2 lg:gap-3 tracking-wide ' + styles.massagerLink}>
               <TelegramFeedback /> Telegram
             </a>
           </div>
-          <input className={'block w-full rounded-full text-base xl:text-lg px-12 py-6 my-6 ' + styles.feedbackInput} placeholder="ОСТАВЬТЕ КОНТАКТ" />
-          <button className={'uppercase px-12 py-6 text-base xl:text-lg font-medium rounded-full ' + styles.cta}>получить консультацию</button>
+          <input className={'block w-full rounded-full text-base xl:text-lg py-3 px-4 sm:px-12 sm:py-6 my-6 ' + styles.feedbackInput} placeholder="ОСТАВЬТЕ КОНТАКТ" />
+          <button className={'uppercase px-8 py-4 sm:px-12 sm:py-6 text-base xl:text-lg font-medium rounded-full ' + styles.cta}>получить консультацию</button>
         </div>
       </div>
       <aside className={'hidden lg:flex flex-1 w-[600px] bg-[url("/Nilita.webp")] bg-no-repeat bg-cover rounded-3xl justify-end gap-5 ' + styles.aside}>
