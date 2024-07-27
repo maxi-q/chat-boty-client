43f106fb02f8c614ce30d67cc5d49d0c6dcc80bf
maxi-q <maximkabust@gmail.com>
2024-07-27 12:41:11 +0300

    footer

--- src/modules/Footer/index.tsx
+++ src/modules/Footer/index.tsx
@@ -5,12 +5,12 @@ import { TelephoneLink, TGLink, VKLink, WhatsAppLink } from '../../constants/sta
 import { Telegram, Vk, WhatsApp } from '../../constants/svg/'
 import styles from './style.module.css'
 
-const liClass = 'opacity-80 laptop:text-xl font-medium laptop:leading-loose'
+const liClass = 'opacity-80 laptop:text-lg desktop:text-xl font-medium laptop:leading-loose desktop:leading-loose'
 const titleClass = 'laptop:text-xl pt-3 pb-6 uppercase tracking-widest'
 
 export const Footer = () => {
   return (
-    <footer className={`${styles.footer} justify-between w-100 flex py-16 desktop:pl-14 px-4 desktop:pe-[148px] gap-[36px] h-max flex-col tracking-wide laptop:flex-row`}>
+    <footer className={`${styles.footer} justify-between w-100 flex py-16 desktop:pl-14 px-4 laptop:pt-8 laptop:pb-8 laptop:px-12 desktop:py-16 desktop:pe-[148px]  gap-[36px] h-max flex-col tracking-wide laptop:flex-row`}>
       <div className={`${styles.logoDiv} overflow-visible phone:w-[409px] laptop:my-8`}>
         <Link href={'/'}>
           <Image src="/logo.webp" alt="chat-boty" width={336} height={228} className={'w-[168px] h-[114px]'} />
