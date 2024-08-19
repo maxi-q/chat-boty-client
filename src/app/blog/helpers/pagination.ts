fe032f61c5c233eca50217bbd8139c85c7afc0ce
maxi-q <maximkabust@gmail.com>
2024-08-19 21:24:56 +0300

    pagination

---
+++ src/app/blog/helpers/pagination.ts
@@ -0,0 +1,21 @@
+export function getPagesNumbs(center: number, end: number): number[] {
+  let start: number = Math.max(1, +center - 2) // Начало диапазона
+  let finish: number = Math.min(end, +center + 2) // Конец диапазона
+
+  // Если размер диапазона меньше 5 чисел, то корректируем start и finish
+  while (finish - start + 1 < 5) {
+    if (start > 1) {
+      start--
+    } else if (finish < end) {
+      finish++
+    }
+  }
+
+  // Создаем массив от start до finish
+  const range: number[] = []
+  for (let i = start; i <= finish; i++) {
+    range.push(i)
+  }
+
+  return range
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,45 +1,60 @@
 import { getArticles } from '@/api/blog/Articles'
-import { Suspense } from 'react'
-import { Card } from './components/Card'
-import ArticlePrev from '@/constants/svg/ArticlePrev'
 import ArticleNext from '@/constants/svg/ArticleNext'
+import ArticlePrev from '@/constants/svg/ArticlePrev'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
+import { Suspense } from 'react'
+import { Card } from './components/Card'
+
+import { getPagesNumbs } from './helpers/pagination'
+import styles from './style.module.css'
 
 const Page = async ({ page }: { page: number }) => {
   if (page < 1) return redirect('?page=1')
 
-  const res = await getArticles({
+  const pageData = await getArticles({
     page: page,
-    size: 8,
+    size: 2,
   })
 
-  if (!res) return <h1>Не удалось получить статьи</h1>
-  if (res.size == 0) return redirect(`?page=${res.total_pages}`)
-  
-  console.log(res)
-  
-  console.log(res, `page: ${page}`)
+  if (!pageData) return <h1>Не удалось получить статьи</h1>
+  if (pageData.size == 0) return redirect(`?page=${pageData.total_pages}`)
 
-  
+
+  const pageNumbs = getPagesNumbs(page, pageData.total_pages)
 
   return (
     <>
-    <div className="flex flex-wrap">
-      {res.data.map((el) => (
-        <Card key={el.id} created_at={el.created_at} duration={el.reading_time} title={el.title} link={el.slug} image={el.slug} />
-      ))}
-    </div>
-    <div className='flex'>
-      <DefaultButton page={+page - 1} direction='left'/>
-      <DefaultButton page={+page + 1} direction='right'/>
-    </div>
+      <div className="flex flex-wrap">
+        {pageData.data.map((el) => (
+          <Card key={el.id} created_at={el.created_at} duration={el.reading_time} title={el.title} link={el.slug} image={el.slug} />
+        ))}
+      </div>
+      <div className="flex">
+        <DefaultButton page={+page - 1} direction="left" />
+        {!pageNumbs.includes(1) && <><NumberButton number={1} /><span className='text-lg pt-2'>...</span></>}
+        
+        {pageNumbs.map((el, i) => <NumberButton key={i} number={el} disable={el == page} />)}
+
+        {!pageNumbs.includes(pageData.total_pages) && <><span className='text-lg pt-2'>...</span><NumberButton number={pageData.total_pages} /></>}
+        <DefaultButton page={+page + 1} direction="right" />
+      </div>
     </>
   )
-} 
+}
 
+const NumberButton = ({ number, disable }: { number: number; disable?: boolean }) => (
+  <Link
+    href={{ query: { page: number } }}
+    className={`flex text-lg font-medium items-center justify-center cursor-pointer no-underline w-[40px] h-[40px] ${disable && styles.selectPage}`}
+  >
+    {number}
+  </Link>
+)
 const DefaultButton: React.FC<{ direction: 'left' | 'right'; page: number }> = ({ direction, page }) => (
-  <Link href={{query: {page: page}}} className={`p-2 cursor-pointer ${direction === 'left' ? 'ml-2' : 'mr-2'}`}>{direction === 'left' ? <ArticlePrev /> : <ArticleNext />}</Link>
+  <Link href={{ query: { page: page } }} className={`p-2 cursor-pointer ${direction === 'left' ? 'ml-2' : 'mr-2'}`}>
+    {direction === 'left' ? <ArticlePrev /> : <ArticleNext />}
+  </Link>
 )
 
 const Blog = ({ searchParams: { page } }: { searchParams: { page: number } }) => (
