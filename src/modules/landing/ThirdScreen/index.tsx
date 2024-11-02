8f95d409e2541e0823e45ed4d4d9abd3d1822f80
maxi-q <maximkabust@gmail.com>
2024-11-02 22:52:10 +0300

    little fix

--- src/modules/landing/ThirdScreen/index.tsx
+++ src/modules/landing/ThirdScreen/index.tsx
@@ -8,7 +8,7 @@ import styles from './style.module.css'
 export const ThirdScreen = () => {
   return (
     <main
-      className={`section before:bg-[url("/aside_abstract.webp")] px-4 py-5 laptop:px-[40px] laptop:pt-[70px] pb-24 before:bg-no-repeat  before:bg-contain before:content-[''] before:absolute before:h-[100%] before:max-h-[20%] before:max-h-[30%] before:w-[100%] laptop:before:max-h-[100%] laptop:before:h-[60%] laptop:before:w-[50%] before:right-[0] before:bottom-[12%] sm:before:bottom-[7%] before:bg-right-bottom laptop:before:bg-bottom laptop:before:right-[0] laptop:before:bottom-[0] ${styles['rotating-background']} ${styles.screen}`}
+      className={`section before:bg-[url("/aside_abstract.webp")] px-4 py-5 laptop:px-[40px] laptop:pt-[70px] pb-24 before:bg-no-repeat  before:bg-contain before:content-[''] before:absolute before:h-[100%] before:max-h-[30%] before:w-[100%] laptop:before:max-h-[100%] laptop:before:h-[60%] laptop:before:w-[50%] before:right-[0] before:bottom-[12%] sm:before:bottom-[7%] before:bg-right-bottom laptop:before:bg-bottom laptop:before:right-[0] laptop:before:bottom-[0] ${styles['rotating-background']} ${styles.screen}`}
     >
       <h1 className={`relative text-3xl phone:text-4xl tablet:text-6xl laptop:text-5xl desktop:text-6xl pb-16 uppercase tracking-wide ${styles.title}`}>
         С Hами <br /> вы забудете о:
@@ -26,7 +26,7 @@ export const ThirdScreen = () => {
       </ul>
 
       <h1 className={`relative text-3xl phone:text-4xl tablet:text-6xl laptop:text-5xl desktop:text-6xl pb-8 pt-32 uppercase tracking-wide ${styles.subtitle}`}>и получите:</h1>
-      <ul className={`flex relative gap-3 max-w-full mx-auto flex-col laptop:flex-row items-center laptop:justify-around h-max items-stretch ${styles.getBox}`}>
+      <ul className={`flex relative gap-3 max-w-full mx-auto flex-col laptop:flex-row laptop:justify-around h-max items-stretch ${styles.getBox}`}>
         <CardGet icon={<GreenFilter />} title={'Воронку / бота'} text={'Которая приносит оплаты / заявки\nс минимальными затратами\nвашего времени'} />
         <CardGet icon={<ChillRock />} title={'Спокойствие'} text={'Заключаем договор и каждый день оповещаем\nо продвижениях\nв вашем проекте'} />
         <CardGet icon={<GreenSettings />} title={'Большой функционал'} text={'Напишем свой скрипт, скопируем ЛЮБУЮ воронку, которая вам\nнравится и сделаем любые интеграции'} />
@@ -36,7 +36,7 @@ export const ThirdScreen = () => {
       <div className="flex relative">
         <aside className={styles.markerResult} />
         <div className={`text-lg phone:text-lg tablet:text-2xl laptop:text-2xl laptop:w-[630px] ${styles.result}`}>
-        Спокойный сон и уверенность, что конкурентов можно легко обойти, ведь подрядчики всё сделали оперативно, без ошибок и с множеством фишек
+          Спокойный сон и уверенность, что конкурентов можно легко обойти, ведь подрядчики всё сделали оперативно, без ошибок и с множеством фишек
         </div>
       </div>
     </main>
