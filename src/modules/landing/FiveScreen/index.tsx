9a6eb971d3e1425b63298ed5e69a2d612d3ab892
Arctik-Senior <kozulka2224.popit@gmail.com>
2026-07-29 20:50:54 +0300

    Update index.tsx

--- src/modules/landing/FiveScreen/index.tsx
+++ src/modules/landing/FiveScreen/index.tsx
@@ -5,7 +5,7 @@ import styles from './style.module.css'
 const FiveScreen = () => {
   return (
     <main className={`section bp-5 px-4 py-5 laptop:px-[40px] laptop:pt-[70px] pb-3 tablet:pb-32 ${styles.screen}`}>
-      <h1 className={`text-4xl laptop:text-5xl desktop:text-6xl ${styles.title}`}>отзывы:</h1>
+      <h2 className={`text-4xl laptop:text-5xl desktop:text-6xl ${styles.title}`}>отзывы:</h2>
       <VideoCarousel />
       <TextCarousel />
     </main>
